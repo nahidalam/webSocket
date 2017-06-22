c063e4c8f8572ccd32627a8ee16de24f60f2ee4a
nahidalam <shapla@gmail.com>
2017-06-21 22:09:20 -0700

    Merge with entire flow

--- client1.js
+++
@@ -1,5 +0,0 @@
-var socket = io();
-socket.emit('broadcast',"Broadcasting Message");
-socket.on('message', function (data) {
-     $('#client1').html(data);
-});
--- client2.js
+++
@@ -1,5 +0,0 @@
-//var socket = io.connect('ws://127.0.0.1');
-var socket = io();
-socket.on('message', function (data) {
-    $('#client2').html(data);
-});
