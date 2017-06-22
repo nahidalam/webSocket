e4fa0f3bcb8cb199dba126adaac850a7148d7ef8
nahidalam <shapla@gmail.com>
2017-06-21 22:31:31 -0700

    changing

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function showHide(){
-  var btn = document.getElementById("btnSubmit");
+  //var btn = document.getElementById("btnSubmit");
   var hiddenitems = document.getElementsByClassName("hidden");
   for (var i =0; i!=hiddenitems.length;i++){
     hiddenitems[i].style.display="inline";
