7edcdcae358074927e39564091d1c5bc809ee5bb
nahidalam <shapla@gmail.com>
2017-06-21 22:36:13 -0700

    changing

---
+++ main.js
@@ -0,0 +1,36 @@
+function showHide(){
+  //var btn = document.getElementById("btnSubmit");
+  var hiddenitems = document.getElementsByClassName("hidden");
+  for (var i =0; i!=hiddenitems.length;i++){
+    hiddenitems[i].style.display="inline";
+  }
+}
+
+function loadInitial(){
+  var hiddenitems = document.getElementsByClassName("hidden");
+  for (var i =0; i!=hiddenitems.length;i++){
+    hiddenitems[i].style.display="none";
+  }
+  var hiddentext = document.getElementsByClassName("hiddenLocalMsg");
+  for (var i =0; i!=hiddentext.length;i++){
+    hiddentext[i].style.display="none";
+  }
+
+}
+
+function doLocal(){
+  var hiddenitems = document.getElementsByClassName("hidden");
+  for (var i =0; i!=hiddenitems.length;i++){
+    hiddenitems[i].style.display="none";
+  }
+  var hiddentext = document.getElementsByClassName("hiddenLocalMsg");
+  for (var i =0; i!=hiddentext.length;i++){
+    hiddentext[i].style.display="inline";
+  }
+}
+function doDist(){
+
+}
+function doGT(){
+
+}
