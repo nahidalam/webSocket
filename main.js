6409ce334bfdd59ee0ad814180c9993bce0db544
nahidalam <shapla@gmail.com>
2017-06-21 23:01:09 -0700

    Local button full function

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ function showHide(){
     hiddenitems[i].style.display="inline";
   }
 }
+function showHideLocal(){
+  //var btn = document.getElementById("btnSubmit");
+  var hiddenitems = document.getElementsByClassName("hiddenLocalButton");
+  for (var i =0; i!=hiddenitems.length;i++){
+    hiddenitems[i].style.display="inline";
+  }
+  var text = document.getElementsByClassName("message");
+  for (var i =0; i!=text.length;i++){
+    text[i].style.display="none";
+  }
+}
 
 function loadInitial(){
   var hiddenitems = document.getElementsByClassName("hidden");
